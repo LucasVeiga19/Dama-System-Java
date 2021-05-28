9886b43c1cae636fe0d52355c4a2b26eee62f67c
LucasVeiga19 <lucas.veiga@laveiga.com.br>
2021-05-28 17:56:56 -0300

    Little improvement in board printing

--- src/dama/Color.java
+++ src/dama/Color.java
@@ -1,6 +1,6 @@
 package dama;
 
 public enum Color {
-    BLACK,
-    WHITE;
+    BLUE,
+    RED;
 }
--- src/dama/DamaMatch.java
+++ src/dama/DamaMatch.java
@@ -27,8 +27,30 @@ public class DamaMatch {
     }
 
     private void initialSetup(){
-        placeNewPiece('b', 6, new Stone(board, Color.WHITE));
-        placeNewPiece('e', 8, new Stone(board, Color.BLACK));
-        placeNewPiece('e', 1, new Stone(board, Color.WHITE));
+        placeNewPiece('a', 1, new Stone(board, Color.RED));
+        placeNewPiece('c', 1, new Stone(board, Color.RED));
+        placeNewPiece('e', 1, new Stone(board, Color.RED));
+        placeNewPiece('g', 1, new Stone(board, Color.RED));
+        placeNewPiece('b', 2, new Stone(board, Color.RED));
+        placeNewPiece('d', 2, new Stone(board, Color.RED));
+        placeNewPiece('f', 2, new Stone(board, Color.RED));
+        placeNewPiece('h', 2, new Stone(board, Color.RED));
+        placeNewPiece('a', 3, new Stone(board, Color.RED));
+        placeNewPiece('c', 3, new Stone(board, Color.RED));
+        placeNewPiece('e', 3, new Stone(board, Color.RED));
+        placeNewPiece('g', 3, new Stone(board, Color.RED));
+
+        placeNewPiece('b', 8, new Stone(board, Color.BLUE));
+        placeNewPiece('d', 8, new Stone(board, Color.BLUE));
+        placeNewPiece('f', 8, new Stone(board, Color.BLUE));
+        placeNewPiece('h', 8, new Stone(board, Color.BLUE));
+        placeNewPiece('a', 7, new Stone(board, Color.BLUE));
+        placeNewPiece('c', 7, new Stone(board, Color.BLUE));
+        placeNewPiece('e', 7, new Stone(board, Color.BLUE));
+        placeNewPiece('g', 7, new Stone(board, Color.BLUE));
+        placeNewPiece('b', 6, new Stone(board, Color.BLUE));
+        placeNewPiece('d', 6, new Stone(board, Color.BLUE));
+        placeNewPiece('f', 6, new Stone(board, Color.BLUE));
+        placeNewPiece('h', 6, new Stone(board, Color.BLUE));
     }
 }
